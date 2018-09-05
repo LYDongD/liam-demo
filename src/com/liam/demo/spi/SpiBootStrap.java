6dea7db6ac250df7d3e7ca13e97e77545f7ab72d
lee <554050334@qq.com>
2018-09-05 14:26:40 +0800

    ignore unneccesary file

--- src/com/liam/demo/spi/SpiBootStrap.java
+++ src/com/liam/demo/spi/SpiBootStrap.java
@@ -2,13 +2,19 @@ package com.liam.demo.spi;
 
 import java.util.List;
 import java.util.ServiceLoader;
+import java.util.TimeZone;
 
 public class SpiBootStrap {
 
+//    public static void main(String args[]) {
+//        ServiceLoader<SpiService> spiServiceList = ServiceLoader.load(SpiService.class);
+//        for (SpiService service : spiServiceList) {
+//            service.print();
+//        }
+//    }
+
     public static void main(String args[]) {
-        ServiceLoader<SpiService> spiServiceList = ServiceLoader.load(SpiService.class);
-        for (SpiService service : spiServiceList){
-            service.print();
-        }
+        System.out.println(TimeZone.getDefault().getID());
+        System.out.println(System.currentTimeMillis());
     }
 }
