ae4c538776ee172aadeeb3e3a15ddaeb58e5db19
lee <554050334@qq.com>
2018-09-15 13:05:21 +0800

    modify proxy demo

--- src/com/liam/demo/proxy/cglib/CglibLoggerInterceptor.java
+++ src/com/liam/demo/proxy/cglib/CglibLoggerInterceptor.java
@@ -6,6 +6,14 @@ import net.sf.cglib.proxy.MethodProxy;
 
 import java.lang.reflect.Method;
 
+/**
+ * The {@code CglibLoggerInterceptor}
+ * 基于类的动态代理，目标对象无需实现接口
+ * 拦截器具有被代理对象的控制权
+ *
+ * @author  liam
+ * @version 1.0
+ */
 public class CglibLoggerInterceptor implements MethodInterceptor{
 
     private Object target;
